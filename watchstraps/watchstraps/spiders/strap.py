28fefc371ef2498d5d0338d254b33f0226cfbce8
husakki <hueschromo@gmail.com>
2025-08-11 20:50:59 +0200

    first working crawl spidy
    with some small issues on the regex, it also scraps some unwanted urls

---
+++ watchstraps/watchstraps/spiders/strap.py
@@ -0,0 +1,22 @@
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+
+class StrapSpider(CrawlSpider):
+    name = "strap"
+    allowed_domains = ["www.watch-tools.de"]
+    start_urls = ["https://www.watch-tools.de"]
+    
+    rules = (
+        # find the strap types
+        Rule(LinkExtractor(allow=(r"\(straps\.php\)",))),
+        # find the straps sizes
+        Rule(LinkExtractor(allow=(r"[a-zA-Z]+-straps/([0-9]+mm[-up]*|[a-zA-Z0-9-]+).php",))),
+        # its a product page
+        Rule(LinkExtractor(allow=(r"[a-zA-Z]+-straps/([0-9]+mm[-up]*|[a-zA-Z0-9-]+)/([a-zA-Z0-9-]+.php)",)), callback ="parse_strap")
+        
+    )
+
+    def parse_strap(self, response):
+        pass
