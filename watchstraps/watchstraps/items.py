47fd336cc7ff166662d3f25c6bbe5cf74872c736
husakki <hueschromo@gmail.com>
2025-08-12 15:33:13 +0200

    Todos done, added output folder

--- watchstraps/watchstraps/items.py
+++ watchstraps/watchstraps/items.py
@@ -8,16 +8,14 @@ from itemloaders.processors import MapCompose, TakeFirst
 from scrapy.item import Field, Item
 from w3lib.html import remove_tags
 
-# TODO price remove the currency symbol 
-# TODO description clean up the whitespace and newlines
 
 class Watchstrap(Item):
     name = Field(
         input_processor= MapCompose(remove_tags),
         output_processor= TakeFirst())
     price = Field(
-        input_processor= MapCompose(remove_tags),
+        input_processor= MapCompose(remove_tags, lambda x: x.replace('€', '').strip()),
         output_processor= TakeFirst())
     description = Field(
-        input_processor= MapCompose(remove_tags),
+        input_processor= MapCompose(remove_tags, lambda x: x.strip()),
         output_processor= TakeFirst())
